384491c6d202f3048e7ad64ec6dc9fa428a50d33
cch <choechanghyeon@MacBook-Pro.local>
2025-10-09 22:11:59 +0900

    10-09

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,15 +1,16 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
-import books from "@/mock/books.json";
 import { BookData } from "@/types";
 
 async function AllBooks() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`, {
+    cache: "no-store",
+  });
   if (!response.ok) {
     return <div>오류가 발생했습니다...</div>;
   }
   const allBooks: BookData[] = await response.json();
-
+   
   return (
     <div>
       {allBooks.map((book) => (
@@ -20,7 +21,11 @@ async function AllBooks() {
 }
 
 async function RecoBooks() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,{
+   next:{
+    revalidate: 3
+   }
+  });
   if (!response.ok) return <div>오류가 발생했습니다...</div>;
 
   const recoBooks: BookData[] = await response.json();
@@ -39,7 +44,7 @@ export default function Home() {
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <RecoBooks/>
+        <RecoBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,15 +1,16 @@
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { BookData } from "@/types";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{ q?: string }>;
-}) {
+export default async function Page({ searchParams }: { searchParams: Promise<{ q?: string }> }) {
   const { q } = await searchParams;
 
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`)
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`);
 
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+
+  const books: BookData[] = await response.json();
   return (
     <div>
       {books.map((book) => (
