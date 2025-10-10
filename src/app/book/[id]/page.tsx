e717b85cedebbf2c0c3bd4385baffb012d30095b
ckdgus222 <ckdgus1308@naver.com>
2025-10-10 17:36:09 +0900

    10-10

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,17 +1,37 @@
+import { notFound } from "next/navigation";
 import style from "./page.module.css";
 
-export default async function Page({ params }: { params: Promise<{ id: string | string[] }> }) {
+// export const dynamicParams = false
+
+export function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+}
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string | string[] }>;
+}) {
   const { id } = await params;
 
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`
+  );
 
-  if (!response.ok) return <div>오류가 발생했습니다...</div>;
+  if (!response.ok){
+    if(response.status === 404){
+      notFound();
+    }
+  }
   const book = await response.json();
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
 
   return (
     <div className={style.container}>
-      <div className={style.cover_img_container} style={{ backgroundImage: `url('${coverImgUrl}')` }}>
+      <div
+        className={style.cover_img_container}
+        style={{ backgroundImage: `url('${coverImgUrl}')` }}
+      >
         <img src={coverImgUrl} />
       </div>
       <div className={style.title}>{title}</div>
