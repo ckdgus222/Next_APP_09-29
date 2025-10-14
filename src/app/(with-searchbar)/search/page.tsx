54fc67a5000581bc01ecf0f1506cf8cf3f007602
ckdgus222 <ckdgus1308@naver.com>
2025-10-14 17:42:25 +0900

    10-14 스트리밍 시작

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,6 +1,7 @@
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
 
+
 export default async function Page({ searchParams }: { searchParams: Promise<{ q?: string }> }) {
   const { q } = await searchParams;
 
